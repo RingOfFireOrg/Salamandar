82af0349f4e1394fdaa70c735f17bc0e210174af
Kylebrown9 <slaveofschool@gmail.com>
2014-02-01 13:54:55 -0500

    Kyles_Cool_Code
    Uses compressor, solenoid, gyro, and meccanum

--- src/edu/wpi/first/wpilibj/templates/Salamander.java
+++ src/edu/wpi/first/wpilibj/templates/Salamander.java
@@ -4,12 +4,13 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
 
-
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -23,93 +24,131 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Salamander extends SimpleRobot {
-     boolean gearboxstate = false, b, a, prevalue = false;
-    //Jags
-      Jaguar leftDrive1 = new Jaguar(2);
-      Jaguar rightDrive1 = new Jaguar(1);
-    //Joystick
-      Joystick rightJoy = new Joystick(1);
-      Joystick leftJoy = new Joystick(2);
-    //Buttons
-      
-      JoystickButton leftTrig = new JoystickButton(leftJoy,1);    
-      JoystickButton rightTrig = new JoystickButton(rightJoy,1);
-    //int
-      double speedVal =0.5;
 
+    //Joysticks
+    Joystick controlStick = new Joystick(2);
+    Joystick driveStick = new Joystick(1);
+    JoystickButton retractButton = new JoystickButton(controlStick, 2);
+    JoystickButton expandButton = new JoystickButton(controlStick, 3);
+
+    //Drive
+    RobotDrive myDrive = new RobotDrive(1, 2, 3, 4);
+
+    //Gyro
+    Gyro itsAGyro = new Gyro(1);
     
-    
-    public void autonomous() 
-        {
-        SmartDashboard.putString("Mode:", " Auto");
-        leftDrive1.set((0.2)*(-1));
-        rightDrive1.set((0.2));
-        Timer.delay(1);
-        leftDrive1.set(0);
-        rightDrive1.set(0);  
+    //Motors
+    Jaguar wis = new Jaguar(5);
+
+    //Pneumatics
+    //DigitalInput pressureSwitch = new DigitalInput(1);                          //the digital input for the pressure switch on the pneumatics board
+    Compressor compressor = new Compressor(1, 1);                               //the compressor which charges air for the storage tanks
+    MySolenoid Kicker = new MySolenoid(1, 2, 3);                                //an arbitrary piston that uses the MySolenoid system
+    //Relay compressorRelay = new Relay(1);
+
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public double rampmotor(double req, double cur) { //init variables Requested speed and Current speed
+        double error = Math.abs(req - cur); //sets variable error to Requested speed minus Current speed
+        double output = 0.0;
+
+        if (error >= 0.1) {
+            output = ((0.1) * (req - cur));
+        } else {
+            output = req;
         }
-    
-    
-    
-    public void operatorControl() 
-        {
-           SmartDashboard.putString("Mode:"," Enabled");
-           while(isEnabled() && isOperatorControl())
-           {
-               Timer.delay(0.1);
+        return output;
+    }
 
-               leftDrive1.set((leftJoy.getY())*(speedVal)*(-1));
-               rightDrive1.set((rightJoy.getY())*(speedVal));
-               
-               
-               
-               
- //Software Gearbox
-                    if (leftTrig.get() && rightTrig.get())
-                      {
-                          a = true;          // b if both buttons pressed
-                      } 
-                    else    
-                      {
-                          a = false;
-                      }
-                    if (!prevalue && a){
-                        gearboxstate=!gearboxstate;
-                    }
-                    if (gearboxstate){
-                        SmartDashboard.putString("Gear:", " HIGH");
-                        speedVal =1;
-                    } else {
-                        SmartDashboard.putString("Gear:", " LOW");
-                       speedVal =0.5;
-                    }
-                    if (a){
-                        prevalue = true;
-                    } else {
-                        prevalue = false;
-                    }
-               
-             
-               
-               
-               
-               
-               
-               
-               
-           }
+    public void autonomous() {
+        myDrive.mecanumDrive_Cartesian(0.5, 0, 0, 0);                           //drive forward at half speed
+        Timer.delay(1);                                                         //wait one second
+        myDrive.mecanumDrive_Cartesian(0, 0, 0, 0);                             //do not move
+    }
 
-       }
-    
-    
-    
-    
-        public void disabled() 
-        {
-        SmartDashboard.putString("Mode:"," Disabled");
-        speedVal =0.5;
-        //Print Disabled to dashboard
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+        //Inintial Compressor Values
+        boolean pressureSwitchVal = true, pressureSwitchPreval = true;
+        compressor.start();
+        //compressorRelay.set(Relay.Value.kForward);
+        //Solenoid system value
+        boolean retractedState = true, extendedState = false, retractButtonVal, expandButtonVal;
+        boolean pistonPrevalueR = true, pistonPrevalueE = false;
+
+        double gyroStatis;
+        
+        double wisVal = 0;
+
+        while (isOperatorControl() && isEnabled()) {
+            Timer.delay(0.01);
+            
+            
+            double z = driveStick.getZ()*-1;
+            if (Math.abs(z) < 0.5) {
+                z = 0;
+            } else if (z > 0) {
+                z -= 0.5;
+            } else {
+                z += 0.5;
+            }
+            gyroStatis = itsAGyro.getAngle();
+            myDrive.mecanumDrive_Cartesian(driveStick.getX(), z, driveStick.getY()*-1, gyroStatis);
+
+            //Compressor
+            pressureSwitchVal = compressor.getPressureSwitchValue();
+
+            if (pressureSwitchVal && !pressureSwitchPreval) {                                          //
+                compressor.stop();
+
+            } else if (!pressureSwitchVal && pressureSwitchPreval) {
+                compressor.start();
+
+            }
+
+            pressureSwitchPreval = pressureSwitchVal;
+
+//Solenoid Actuating
+            retractButtonVal = retractButton.get();
+            expandButtonVal = expandButton.get();
+
+//determines the desired state of the system based on buttons
+            if (retractButtonVal && !expandButtonVal) {
+                retractedState = true;
+                extendedState = false;
+                SmartDashboard.putString("Kicker:", " Retracted");
+            } else if (!retractButtonVal && expandButtonVal) {
+                retractedState = false;
+                extendedState = true;
+                SmartDashboard.putString("Kicker:", " Extended");
+            } else if (retractButtonVal && expandButtonVal) {
+                SmartDashboard.putString("Kicker:", " Both bottons pressed");
+            }
+
+//Extends and Retracts the piston only when not previously extended or retracted
+            if (retractedState && !pistonPrevalueR) {
+                Kicker.retract();
+            } else if (extendedState && !pistonPrevalueE) {
+                Kicker.extend();
+            } else {
+    //nothing
+            }
+
+            pistonPrevalueE = extendedState;
+            pistonPrevalueR = retractedState;
+            wisVal = rampmotor(controlStick.getThrottle(), wisVal);
+            wis.set(wisVal);
         }
-     
-     
+
+    }
+
+    /**
+     * This function is called once each time the robot enters test mode.
+     */
+    public void test() {
+
+    }
 }
